5dcb3f5ba226a7b39459caca2509b457e60066dd
RJ_Stony <joshua052698@gmail.com>
2025-06-17 12:03:46 +0900

    chore: comment out base URL in Vite config for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Feple/", // 꼭 슬래시로 감싸기!
+  // base: "/Feple/", // 꼭 슬래시로 감싸기!
   plugins: [react()],
 });
