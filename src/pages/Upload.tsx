23ba6dd9bbc842bc92eabeb12110e29cb52d9874
RJ_Stony <joshua052698@gmail.com>
2025-07-05 10:57:00 +0900

    add: vite-env.d.ts

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 import { ResultsDisplay } from "../components/ResultsDisplay";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 const pageVariants: Variants = {
